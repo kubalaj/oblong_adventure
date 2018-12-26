ca885677fd46ef28e5cee7a004ea9b28a9b21bc0
kubalaj <jp@jpkubala.com>
2018-12-25 19:56:51 -0700

    adding a gear page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,8 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
-require('./styles.scss')
+
+require('../styles/main.scss')
 
 const IndexPage = () => (
   <Layout>
