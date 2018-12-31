ab730b446293dcb4cb0599121e3cc64cd6322fa5
kubalaj <jp@jpkubala.com>
2018-12-31 09:06:48 -0700

    finding solutions to the font issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-resolve-src',
     'gatsby-transformer-remark',
     {
       resolve: `gatsby-source-filesystem`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet';
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
@@ -10,6 +11,10 @@ require('../styles/main.scss')
 const keywords = ['adventure', 'blog', 'kayak', 'whitewater', 'trail', 'running', 'stories', 'van', 'sking'];
 const IndexPage = () => (
   <Layout>
+    <Helmet>
+      <link href="https://fonts.googleapis.com/css?family=Overpass|Permanent+Marker" rel="stylesheet"></link>
+    </Helmet>
+    <p className="test">test</p>
     <SEO title="Home" keywords={keywords} />
     <Link to="/stories/">stories</Link>
     <Link to="/gear/">gear</Link>
