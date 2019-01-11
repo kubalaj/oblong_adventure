0c5f0e2256ffb98ec1661b7f0c79dfeb04e97b67
kubalaj <jp@jpkubala.com>
2019-01-11 13:15:04 -0800

    finishin up tutorial

---
+++ src/pages/butler-gultch.md
@@ -0,0 +1,6 @@
+---
+title: "Butler Gultch"
+date: "2018-12-30"
+---
+
+Bulter Gultch was rad
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
@@ -7,10 +6,39 @@ import SEO from '../components/seo'
 require('../styles/main.scss')
 
 const keywords = ['adventure', 'blog', 'kayak', 'whitewater', 'trail', 'running', 'stories', 'van', 'sking'];
-const IndexPage = () => (
+export default ({data}) => (
   <Layout>
     <SEO title="Home" keywords={keywords} />
+    <div>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3>
+              {node.frontmatter.title}{" "}
+              <span>
+                — {node.frontmatter.date}
+              </span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </div>
   </Layout>
 )
 
-export default IndexPage
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }`
