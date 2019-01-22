a8e38034765b20a97e6954ffdc258c769efbd0e8
kubalaj <jp@jpkubala.com>
2019-01-22 10:49:58 -0800

    adding a little more look

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -4,6 +4,11 @@ import * as d3 from 'd3'
 import './map.scss'
 
 class Map extends Component {
+  constructor() {
+    super()
+    console.log('test')
+  }
+
   render() {
     return <div>I am a map</div>
   }
