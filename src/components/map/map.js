8d068016e70f44a28d36ef070ee59d3533de953c
kubalaj <jp@jpkubala.com>
2019-01-22 10:26:50 -0800

    adding map dir

---
+++ src/components/map/map.js
@@ -0,0 +1,35 @@
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import './header.scss'
+
+const Header = ({ siteTitle }) => (
+  <div className="header">
+    <div className="container">
+      <h1>
+        <Link to="/">{siteTitle}</Link>
+      </h1>
+    </div>
+  </div>
+)
+// <div className="header">
+//   <div className="container">
+//     <h1>
+//       <Link to="/">{siteTitle}</Link>
+//     </h1>
+//   </div>
+//   <Link to="/stories/">stories</Link>
+//   <Link to="/gear/butler_gultch">gear</Link>
+//   <Link to="/trip-reports/">trip reports</Link>
+// </div>
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: '',
+}
+
+export default Header
